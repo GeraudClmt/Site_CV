c04bbec0fc25c8b3dfc1d1adfd352e7ebad0a292
GeraudClmt <geraud-yakou.clement@le-campus-numerique.fr>
2025-06-18 09:18:44 +0200

    Mise à jour du visuel via Bootstrap

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ header.innerHTML = `
             <div class="col-6 col-lg-8 h-100 text-center p-0">
                 <h1 class="mt-2"> Développeur Avancé</h1>
                 <h2 class="m-0">2025 - 2026</h2>
-                <h2 class="m-0 mt-1">Recherche d'une alternance  du 8 décembre 2025 au 8 décembre 2026</h2>
+                <h2 class="m-0 mt-1">Recherche d'une alternance  du 8 décembre 2025 au 4 décembre 2026</h2>
             </div>
             <div class="col-3 col-lg-2 h-100 p-3 text-center ">
                 <img class="h-100 contourRose img-fluid " src="Assets/QRCode.png" alt="QR Code">
